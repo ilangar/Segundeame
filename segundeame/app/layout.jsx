81cb9118468fe9edb1794e801c47d49ae8a3ca72
Cata <47757025@est.ort.edu.ar>
2024-06-03 17:31:47 -0300

    copiado lo hacho en casa

--- segundeame/app/layout.jsx
+++ segundeame/app/layout.jsx
@@ -13,7 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header className="w-full flex justify-between align-center bg-green-700">
+        <div className="flex align-center justify-center ml-2">
+          <a href="./"><img src="\logo.png" alt="logo de segundeame" className="h-10" /></a>
+        </div>
+        <nav className="flex align-center justify-center">
+          <ul className="flex align-center justify-center">
+            <li className="flex align-center justify-center mr-2"><a href="./perfil" className="text-white align-center justify-center">Perfil</a></li>
+          </ul>
+        </nav>
+    </header>
+        {children}
+        
+      </body>
+
     </html>
   );
 }
---
+++ segundeame/app/materiales/page.jsx
@@ -0,0 +1,5 @@
+export default function MaterialesPage (){
+    return(
+        <p>Busca tus materiales</p>
+    )
+}
