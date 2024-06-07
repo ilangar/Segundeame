131ffab7a806ae2e7d1d5af82398a00dd5d986e0
ilangar <110467573+ilangar@users.noreply.github.com>
2024-06-07 10:22:35 -0300

    avance

--- segundeame/app/index.js
+++ segundeame/app/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import route from './routes/materiales.route'
+import route from './routes/materiales.route.js'
 
 const app = express()
 
--- segundeame/app/routes/materiales.route.js
+++ segundeame/app/routes/materiales.route.js
@@ -1,6 +1,19 @@
-import { Router } from "express"
+import { Router } from "express";
+import {PrismaClient} from '@prisma/client'
 
 const router = Router()
+const prisma = new PrismaClient();
 
+router.get('/materiales', async (req, res) =>{
+    const material = await prisma.material.findMany()
+    res.json(material)
+})
+
+router.post('/materiales', async (req, res) => {
+    const newMaterial = await prisma.material.create({
+        data: req.body, 
+    })
+    res.json(newMaterial)
+})
 
 export default router;
