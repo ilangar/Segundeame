edb97690d5cf12b2362224d7a442117c35b60508
Cata <47757025@est.ort.edu.ar>
2024-08-05 12:02:34 -0300

    implementación del diseño de Ong y subir ong

--- segundeame/app/(ong)/ong/page.jsx
+++ segundeame/app/(ong)/ong/page.jsx
@@ -2,10 +2,18 @@ export default function ONGs() {
   
     return (
      
-    <div>
-      {/* Resto del contenido de tu página */}
-      <h1>Ayuda a estas ong</h1>
-    </div>
+      <section className="flex align-center justify-center">
+      <div className="flex align-center justify-center">
+        
+        <div className="absolute aling-center bottom-10 bg-[#80B48B] hover:bg-[#6C9675] focus:outline-none focus:bg-green-300 rounded-md py-2 px-4">
+          <a href="./subir-ong" className="text-white">Subir nuevo pedido</a>
+        </div>
+        
+        
+            
+          
+      </div>
+    </section>
   
        
     );
---
+++ segundeame/app/(ong)/subir-ong/page.jsx
@@ -0,0 +1,55 @@
+export default function ONGs() {
+  
+    return (
+     
+        <main className="">
+        <div className=" mt-10 flex align-center justify-center w-full flex-col overflow-hidden">
+            <form className="flex align-center justify-center">
+                <fieldset className="flex align-center justify-center flex-col gap-y-2">
+                    <label className="block border border-[#80B48B] rounded-md py-2 px-4 mr-2">
+                        Imagen (máximo 5)
+                        <input type="file" accept="image/*" multiple className="cursor-pointer" 
+                           
+                        />
+                    </label>
+                    <label className="block border border-[#80B48B] rounded-md py-2 px-4 mr-2">
+                        <input 
+                            type="text" required placeholder="Título*"
+                            className="focus:outline-none"
+                        />
+                    </label>
+                    <label className="block border border-[#80B48B] rounded-md py-2 px-4 mr-2">
+                        <input 
+                            type="text" required placeholder="Descripción*" 
+                            className="focus:outline-none"
+                        />
+                    </label>
+                    <div className="block border border-[#80B48B] rounded-md py-2 px-4 mr-2 ">
+                        <label className="block">Contacto:</label>
+                        <div className="block ">
+                            <label className="block focus:outline-none ">
+                                <input 
+                                    type="email" required placeholder="Email*"
+                                    className="focus:outline-none"
+                                />
+                            </label>
+                            <label className="block">
+                                <input 
+                                    type="tel"  placeholder="Telefono" 
+                                    className="focus:outline-none"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <input 
+                        type="submit" 
+                        value="Subirpedido" 
+                        className=" text-white bg-[#80B48B] hover:bg-[#6C9675] cursor-pointer rounded-md py-2 px-4" 
+                    />
+                </fieldset>
+            </form>
+        </div>
+    </main>
+    );
+  }
+  
