4dcdefcd6de9da66e79bf0d7d49d64c65327cdda
Cata <47757025@est.ort.edu.ar>
2024-08-24 23:26:52 -0300

    IA: Nombre variable y borrado de comentario HOME:  Intento de fondo con tres imagenes
    IA: Cambie el nombre de la variable res a respuestaIA y borre de comentario //hacer un UseSate
    
    HOME:  Intento de fondo con tres imágenes

--- segundeame/app/(ia)/ia/page.jsx
+++ segundeame/app/(ia)/ia/page.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 
 export default function IA() {
 
-  const [res, setRes] = useState("")
+  const [respuestaIA, setRes] = useState("")
   
   const handleSearch = async (inputText) => {
     console.log("Se realizó una búsqueda con el término:", inputText);
@@ -19,7 +19,7 @@ export default function IA() {
       const result = await model.generateContent(prompt);
       const response = result.response;
       console.log(response.candidates[0].content.parts[0].text);
-      setRes(response.candidates[0].content.parts[0].text) //hacer un UseState
+      setRes(response.candidates[0].content.parts[0].text) 
     } catch (error) {
       console.error('Error al generar contenido:', error);
     }
@@ -32,7 +32,7 @@ export default function IA() {
         <ChatIA onSearch={handleSearch} />
         <div className="relative top-20 flex justify-center" >
           <p className="text-center rounded-lg py-4 px-6 bg-[#80B48B] w-full max-w-[90%] md:max-w-[75%] lg:max-w-[60%] max-h-[425px] overflow-y-auto break-words border-[#80B48B] border-8"> 
-            {res} 
+            {respuestaIA} 
           </p>
         </div>
       </div>
--- segundeame/app/page.jsx
+++ segundeame/app/page.jsx
@@ -3,37 +3,32 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Home() {
-  const images = ['bg-[url("/foto1.png")]', 'bg-[url("/foto2.png")]']
+  const images = ['bg-[url("/foto1.png")]', 'bg-[url("/foto2.png")]', 'bg-[url("/foto3.png")]'];
+  
   const [backgroundUrl, setBackgroundUrl] = useState(images[0]);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (backgroundUrl === images[1]) {
-        setBackgroundUrl(images[0]); 
-      }
-      else{
-        setBackgroundUrl(images[1]); 
-      }
+      const currentIndex = images.indexOf(backgroundUrl);
+      const nextIndex = (currentIndex + 1) % images.length;
+      setBackgroundUrl(images[nextIndex]);
     }, 3000);
 
     return () => clearInterval(timer); 
-  }, []);
+  }, [backgroundUrl]);
 
   return (
-  <main className={`absolute -z-20 ${backgroundUrl} h-full w-full`}>
-    <div className="absolute -z-10 bg-gradient-to-r from-black to-transparent h-full w-full"> 
-      <div className="absolute top-12 left-1/2 -translate-x-1/2 items-center bg-transparent h-40 w-full mt-10 pl-8">
-        <img src="\logo.png" alt="logo de segundeame" className="h-20"/>
-        <p className="text-white ml-4 text-xl">Te segundeamos para que vos puedas ayudar al planeta</p>
-      </div>        
-        
-    </div>
-    
-    <div className="ml-12 absolute aling-center bottom-10 bg-[#80B48B] hover:bg-[#6C9675] focus:outline-none focus:bg-green-300 rounded-md py-2 px-4">
-      <a href="./ayuda" className="text-white">¿Cómo usar Segundeame?</a>
-    </div>
-  </main>
-
-     
+    <main className={`absolute -z-20 ${backgroundUrl} h-full w-full`}>
+      <div className="absolute -z-10 bg-gradient-to-r from-black to-transparent h-full w-full"> 
+        <div className="absolute top-12 left-1/2 -translate-x-1/2 items-center bg-transparent h-40 w-full mt-10 pl-8">
+          <img src="\logo.png" alt="logo de segundeame" className="h-20"/>
+          <p className="text-white ml-4 text-xl">Te segundeamos para que vos puedas ayudar al planeta</p>
+        </div>        
+      </div>
+      
+      <div className="ml-12 absolute aling-center bottom-10 bg-[#80B48B] hover:bg-[#6C9675] focus:outline-none focus:bg-green-300 rounded-md py-2 px-4">
+        <a href="./ayuda" className="text-white">¿Cómo usar Segundeame?</a>
+      </div>
+    </main>
   );
 }
