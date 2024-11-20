e9e9f6ef309a00c2d03a2bc24b4ffe454fde7d61
Galu1905 <galrubel19@gmail.com>
2024-11-20 09:32:24 -0300

    Update page.jsx

--- app/(ia)/ia/page.jsx
+++ app/(ia)/ia/page.jsx
@@ -1,9 +1,11 @@
 "use client"
 
+require('dotenv').config();
 const API_KEY = process.env.REACT_APP_API_KEY;
 console.log(API_KEY)
 import ChatIA from "@/app/components/chatIA";
 import { GoogleGenerativeAI } from "@google/generative-ai"
+import ReactDOM from 'react-dom';
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 import { useState } from 'react';
