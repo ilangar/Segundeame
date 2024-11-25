31a121b0a999dd1649fd439c7169c8b28191c365
ilangar <110467573+ilangar@users.noreply.github.com>
2024-11-25 11:21:26 -0300

    Update page.jsx

--- app/(ia)/ia/page.jsx
+++ app/(ia)/ia/page.jsx
@@ -9,7 +9,11 @@ import ReactDOM from 'react-dom';
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 import { useState } from 'react';
-
+const changeMessageWithDelay = () => {
+  setTimeout(() => {
+    setMessage('This message is delayed');
+  }, 20000); // 2000ms = 2 seconds delay
+};
 export default function IA() {
 
   const [respuestaIA, setRes] = useState("")
@@ -35,8 +39,9 @@ export default function IA() {
       <div >
         <ChatIA onSearch={handleSearch} />
         <div className="relative top-20 flex justify-center" >
+          dela
           <p className="text-center rounded-lg py-4 px-6 bg-[#80B48B] w-full max-w-[90%] md:max-w-[75%] lg:max-w-[60%] max-h-[425px] overflow-y-auto break-words border-[#80B48B] border-8"> 
-            {"comenzá cortando las piernas del jean para obtener dos rectángulos grandes que serán las partes frontal y trasera, y una tira larga y ancha (de unos 15 cm de ancho) para los laterales y la base. Si querés bolsillos externos, usá los bolsillos traseros del jean, recortalos y cosélos en el rectángulo frontal. Después, colocá el rectángulo trasero sobre la mesa, uní con alfileres la tira larga que será el lateral y la base, rodeando todo el rectángulo, y cosé las piezas juntas. Luego, uní el rectángulo frontal al otro lado de la tira larga, asegurándote con alfileres antes de coser para formar la estructura básica. En la parte superior de la mochila, colocá un cierre para que puedas abrir y cerrar el compartimento principal. Para las correas, cortá dos tiras largas de jean o usá cinturones viejos, cosiendo un extremo en la parte superior trasera y el otro en la parte inferior trasera. Si querés un acabado más prolijo, podés agregar un forro interior con tela que se ajuste al tamaño de las piezas. "} 
+            {changeMessageWithDelay} 
           </p>
         </div>
       </div>
